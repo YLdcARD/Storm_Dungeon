476c2d2eff47ff3f51e274939b62af1898106107
YLdcARD <89815645+YLdcARD@users.noreply.github.com>
2021-09-07 17:00:35 -0400

    Final 1.2.1

--- STORM_DUNGEON.py
+++ STORM_DUNGEON.py
@@ -152,7 +152,7 @@ while wants_to_play == True:
         player_response = input(f"You choose {player_class}, are you SURE?! (Y/N/Info): ").title()
         if(player_response == "Info"):
             if player_class == "Tank":
-                print("A Tank is a Hardened WARRIOR. A tank is a character whose primary purpose or class role is to absorb damage and prevent others from being attacked. Tanks are "meat shields", so to speak, putting themselves between the mobs and the more vulnerable party members. The tank's task is to hold aggro of the mobs to keep them off the other group members.")
+                print("A Tank is a Hardened WARRIOR. A tank is a character whose primary purpose or class role is to absorb damage and prevent others from being attacked. Tanks are meat shields, so to speak, putting themselves between the mobs and the more vulnerable party members. The tank's task is to hold aggro of the mobs to keep them off the other group members.")
             if player_class == "Mage":
                 print("Magic is a tool, some would even say a gift. But you should never forget its fundamental nature. Arcane magic twists the boundaries of our world. It weakens the laws of this realm to allow the impossible to happen, if only for a moment. That single moment is all a demon needs to wreak havoc. The Burning Legion has watched us for millenia, always waiting for the smallest rift to slip through, the smallest stirring to whisper from. Remember this, young ones. Every spell you cast, no matter how minor, cuts both ways. This is why sorcery must never be used frivolously.")
             if player_class == "Druid":
